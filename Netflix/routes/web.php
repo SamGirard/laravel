4355dd6831a5a8a5f8ce64e5a1ffc23cc46f4497
Samuel Girard <sgirard2004@icloud.com>
2023-10-16 13:23:16 -0400

    test

---
+++ Netflix/routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PokemonsController;
+use App\Http\Controllers\NetflixController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('pokemons',
+[PokemonsController::class, 'index'])->name('pokemons.index');
+
+Route::get('Netflix',
+[NetflixController::class, 'index'])->name('Netflix.index');
