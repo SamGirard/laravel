1ffe409742d498e1c9e2ae47d99fcc4c779549c1
QUEBEC987 <isaac_bd@outlook.com>
2023-11-02 17:39:17 -0400

    allo

--- Netflix/database/seeders/ActeursTableSeeder.php
+++ Netflix/database/seeders/ActeursTableSeeder.php
@@ -22,6 +22,36 @@ class ActeursTableSeeder extends Seeder
                 'portrait' => 'https://prod-images.tcm.com/Master-Profile-Images/LeonardoDiCaprio.jpg',
                 'age' => 48
             ],
+            [
+                'id' => 2,
+                'nom' => 'Al Pacino',
+                'dateNaissance' => '25 avril 1940',
+                'lieuNaissance' => 'Spanish Harlem, New York, État de New York, États-Unis',
+                'portrait' => 'https://upload.wikimedia.org/wikipedia/commons/9/98/Al_Pacino.jpg',
+                'age' => 83
+            ],
+            [
+                'id' => 3,
+                'nom' => 'Marlon Brando',
+                'dateNaissance' => '3 avril 1924',
+                'lieuNaissance' => 'Omaha, Nebraska, États-Unis',
+                'portrait' => 'https://t3.gstatic.com/licensed-image?q=tbn:ANd9GcQ5-5i-xd0ny8Rk_Ck1C803Tdg4uIrXDBJPSjQbjQcgWmi6fJcBR-E3hFfYBu7yd-Yi',
+                'age' => 43
+            ],
+
+
+
+/*
+            [
+                'id' => 1,
+                'nom' => '',
+                'dateNaissance' => '',
+                'lieuNaissance' => '',
+                'portrait' => '',
+                'age' => 
+            ],
+
+            */
         ]);
     }
 }
