b052633aaa4096797a122665512b3493d1941776
QUEBEC987 <isaac_bd@outlook.com>
2023-11-13 13:06:35 -0500

    Table seeder

---
+++ Netflix/database/seeders/UsagersTableSeeder.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class TableSeeder extends Seeder
+{
+    public function run(): void
+    {
+        DB::table('uagers')->insert([
+            [
+                'id' => 1,
+                'nom' => 'admin',
+                'prenom' => 'admin',
+                'username' => 'admin',
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make($motDePasse),
+                'role' => 'admin'
+            ],
+            [
+                'id' => 2,
+                'nom' => 'Séguin',
+                'prenom' => 'Félix',
+                'username' => 'Felix1',
+                'email' => 'felix@gmail.com',
+                'password' => Hash::make($motDePasse),
+                'role' => 'normal'
+            ],
+            [
+                'id' => 3,
+                'nom' => 'Scott',
+                'prenom' => 'Antoine',
+                'username' => 'Tony123',
+                'email' => 'tony_sc@gmail.com',
+                'password' => Hash::make($motDePasse),
+                'role' => 'enfant'
+            ],
+            /*
+            [
+                'id' => ,
+                'nom' => '',
+                'prenom' => '',
+                'username' => '',
+                'email' => '',
+                'password' => Hash::make($motDePasse),
+                'role' => ''
+            ],
+            */
+        ]);
+    }
+}
