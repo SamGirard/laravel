02dcfa6bb99a40591090dd286acb486b9093bf82
QUEBEC987 <isaac_bd@outlook.com>
2023-11-20 12:56:12 -0500

    a

--- Netflix/resources/views/Netflix/personne.blade.php
+++ Netflix/resources/views/Netflix/personne.blade.php
@@ -5,20 +5,19 @@
     @section('contenu')
     @auth
     <div class="row">
-        <div class="row__posterss">
-            @if(count($personnes))
-                @foreach($personnes as $personne)
-                    <div class="image-container">
-                      <a href="{{route('Netflix.zoom', [$personne]) }}">
-                        <img src="{{$personne->portrait}}" class="row_poster row_posterLarge portrait">
-                      </a>
-                    <h3 class="cNom">{{$personne->nom}}</h3>    
-                </div>
-                @endforeach
-            @else
-            @endif
-  </div>
-</div>
+      <div class="row__posterss">
+        @if(count($personnes))
+          @foreach($personnes as $personne)
+            <div class="image-container">
+              <a href="{{route('Netflix.zoom', [$personne]) }}">
+                <img src="{{$personne->portrait}}" class="row_poster row_posterLarge portrait">
+              </a>
+                  <h3 class="cNom">{{$personne->nom}}</h3>    
+            </div>
+          @endforeach
+        @endif
+      </div>
+    </div>
 
 
 
