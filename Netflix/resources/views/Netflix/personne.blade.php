323217181e6d67085d34064ca3b73d2589d43ef1
QUEBEC987 <isaac_bd@outlook.com>
2023-11-27 13:33:10 -0500

    a

--- Netflix/resources/views/Netflix/personne.blade.php
+++ Netflix/resources/views/Netflix/personne.blade.php
@@ -5,24 +5,6 @@
     @section('contenu')
     @auth
 
-    <!--
-      L'ancien format en forme de boule
-    <div class="row">
-      <div class="row__posterss">
-        @if(count($personnes))
-          @foreach($personnes as $personne)
-            <div class="image-container">
-              <a href="{{route('Netflix.zoom', [$personne]) }}">
-                <img src="{{$personne->portrait}}" class="row_poster row_posterLarge portrait">
-              </a>
-                  <h3 class="cNom">{{$personne->nom}}</h3>    
-            </div>
-          @endforeach
-        @endif
-      </div>
-    </div>
-  -->
-
   <!--
   <div class="row">
     <div class="row__posterss">
@@ -42,18 +24,53 @@
   </div>
   -->
 
-  
+  <h1 class="sexe">Hommes</h1>
   <div class="row">
-  <h2 class="sexe">Hommes</h2>
     <div class="row__posterss">
           @if(count($hommes))
             @foreach($hommes as $homme)
             <div class="card" >
               <a href="{{route('Netflix.zoom', [$homme]) }}">
-                <img src="{{$homme->portrait}}" class="card-img-top row_poster row_posterLarge">
+                <img src="{{$homme->portrait}}" class="row_poster row_posterLarge">
               </a>
               <div class="card-body">
                 <h3 class="card-title cNomPersonne">{{$homme->nom}}</h5>
+              </div>
+            </div>
+            @endforeach
+          @endif
+    </div>
+  </div>
+
+  <h1 class="sexe">Femmes</h1>
+  <div class="row">
+    <div class="row__posterss">
+          @if(count($femmes))
+            @foreach($femmes as $femme)
+            <div class="card" >
+              <a href="{{route('Netflix.zoom', [$femme]) }}">
+                <img src="{{$femme->portrait}}" class="card-img-top row_poster row_posterLarge">
+              </a>
+              <div class="card-body">
+                <h3 class="card-title cNomPersonne">{{$femme->nom}}</h5>
+              </div>
+            </div>
+            @endforeach
+          @endif
+    </div>
+  </div>
+
+  <h1 class="sexe">Autres</h1>
+  <div class="row">
+    <div class="row__posterss">
+          @if(count($autres))
+            @foreach($autres as $autre)
+            <div class="card" >
+              <a href="{{route('Netflix.zoom', [$autre]) }}">
+                <img src="{{$autre->portrait}}" class="card-img-top row_poster row_posterLarge">
+              </a>
+              <div class="card-body">
+                <h3 class="card-title">{{$autre->nom}}</h5>
               </div>
             </div>
             @endforeach
